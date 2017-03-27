121c1701fbb37f00b655e0adb61536516f40ea6e
Sober <15821589161@163.com>
2017-03-27 17:32:28 +0800

    修改文档显示格式，加入了运行时权限申请

--- app/src/main/java/com/ly/avid/avidaccountkitdemo/MainActivity.java
+++ app/src/main/java/com/ly/avid/avidaccountkitdemo/MainActivity.java
@@ -1,9 +1,19 @@
 package com.ly.avid.avidaccountkitdemo;
 
+import android.Manifest;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
@@ -31,6 +41,9 @@ public class MainActivity extends AppCompatActivity {
     public static int APP_REQUEST_CODE = 99;
     private Context mContext;
     CallbackManager callbackManager;
+
+    //是否跳转过应用程序信息详情页
+    private boolean mIsJump2Settings = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -128,12 +141,87 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.btn_login_guest).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                uploadGameUserInfo("sober_guest", AvidAccoundType.TYPE_GUEST, null);
+                showMsg("SDK_INT:"+Build.VERSION.SDK_INT+", M:"+Build.VERSION_CODES.M);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (!(checkSelfPermission(Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED)) {
+                        if (shouldShowRequestPermissionRationale(Manifest.permission.READ_PHONE_STATE)) {
+                            showMsg("Please grant the permission this time");
+                        }
+                        requestPermissions(new String[]{Manifest.permission.READ_PHONE_STATE, Manifest.permission.READ_SMS}, 1);
+                    }else {
+                        getDeviceId();
+                    }
+                }else {
+                    getDeviceId();
+                }
+                //uploadGameUserInfo("sober_guest", AvidAccoundType.TYPE_GUEST, null);
             }
         });
     }
 
-    private void uploadGameUserInfo(String gameAccountId, String gameAccountType, String gameAccountToken){
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mIsJump2Settings) {
+            onRecheckPermission();
+            mIsJump2Settings = false;
+        }
+    }
+
+    //如果从设置界面返回，则重新申请权限
+    public void onRecheckPermission() {
+        showMsg("onRecheckPermission");
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1) {
+            int grantResult = grantResults[0];
+            if (grantResult == PackageManager.PERMISSION_GRANTED) {
+                getDeviceId();
+            }else {
+                if (!ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.READ_PHONE_STATE)) {
+                    //showMsg("open app setting to open the permission");
+                    showPermissionSettingsDialog();
+                }
+            }
+        }
+    }
+
+    private void showPermissionSettingsDialog() {
+        new AlertDialog.Builder(this).setCancelable(false).setTitle("温馨提示").
+                setMessage("由於您已勾選不再提示該權限授權，如需登錄之前的遊客角色，請打開權限：設置－應用－怪物X聯盟2－權限－電話，謝謝！").setNegativeButton("下次吧", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        }).setPositiveButton("去设置", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                jump2PermissionSettings();
+            }
+        }).show();
+    }
+
+    /**
+     * 跳转到应用程序信息详情页面
+     */
+    private void jump2PermissionSettings() {
+        mIsJump2Settings = true;
+        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        intent.setData(Uri.parse("package:" + getPackageName()));
+        startActivity(intent);
+    }
+
+    private void getDeviceId() {
+        TelephonyManager telephonyManager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+        String deviceId = telephonyManager.getDeviceId();
+        showMsg("获取设备ID:, "+deviceId);
+    }
+
+    private void uploadGameUserInfo(String gameAccountId, String
+            gameAccountType, String gameAccountToken){
         AvidUserManager.uploadGameUserInfo(gameAccountId, gameAccountType, gameAccountToken, new AvidAccountUploadCallback(){
             @Override
             public void onSuccess(String gameAccountId, String avid) {
